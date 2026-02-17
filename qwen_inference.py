571e5d8ea83fc4c018ef4a6085e8b8fda930ea47
Adexandria <adeolaaderibigbe09@gmail.com>
2026-02-17 17:59:15 +0100

    Supports video understanding

--- qwen_inference.py
+++ qwen_inference.py
@@ -1,46 +1,50 @@
-import torch
-from decord import VideoReader
-from transformers import  Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
 from qwen_vl_utils import process_vision_info
+import re
 import numpy as np
 
-
-def inference(video_path, prompt):
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    video = VideoReader(video_path)
-
-    total_frames = len(video)
+def inference(model, processor, video_path, prompt, device):
 
     # Simulate inference results
     messages =[
         {"role": "user",
         "content": [
              { 
-                "video": video_path,
-                "total_pixels": 20480 * 32 * 32,
-                "min_pixels": 64 * 32 * 32,
-                "max_frames": total_frames,
-                "samples_fps": 2},
-            {"type": "text", "content": prompt}
+               "video" : video_path,
+                "max_pixels": 360 *420,
+                "fps": 2.0
+               },
+            {"type": "text", "text": prompt}
         ],
         }
     ]
-    model = Qwen2VLForConditionalGeneration.from_pretrained("Qwen/Qwen2-VL-2B-Instruct",torch_dtype=torch.float16,device_map = device)
-    processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-2B-Instruct")
 
     text = processor.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-    image_inputs, video_inputs, video_kwargs = process_vision_info([messages], return_video_kwargs=True)
 
-    if video_kwargs:
-        for key, value in video_kwargs.items():
-            if isinstance(value, list) and len(value) > 0:
-                video_kwargs[key] = value[0]
-    inputs = processor(text=[text], images=image_inputs, videos=video_inputs,**video_kwargs, return_tensors="pt")
+    image_inputs, video_inputs = process_vision_info([messages])
 
-    inputs = inputs.to(device)
+    inputs = processor(text=[text], images=image_inputs, videos=video_inputs, padding=True, return_tensors="pt").to(device)
 
-    output_ids = model.generate(**inputs, max_new_tokens=256)
+    output_ids = model.generate(**inputs, max_new_tokens=128, do_sample=False)
+
     generated_ids = [output_ids[len(input_ids):] for input_ids, output_ids in zip(inputs.input_ids, output_ids)]
-    output_text = processor.batch_decode(generated_ids, skip_special_token=False, clean_up_tokenization_spaces=False)
-    return output_text[0]
+
+    output_text = processor.batch_decode(generated_ids, skip_special_token=True, clean_up_tokenization_spaces=True)
+
+    return extract_item(output_text[0])
+    
+
+def extract_item(response):
+
+    clean_text = response.split("<|im_end|>")[0].strip()
+
+    patterns = [
+        r"is (?:a|an|the) (.*)", 
+        r"lifted is (.*)",
+        r"object is (.*)"
+    ]
+    for pattern in patterns:
+        match = re.search(pattern, clean_text, re.IGNORECASE)
+        if match:
+            return match.group(1).strip(" .")
+    
+    return clean_text.strip("")
--- video_pipeline.py
+++ video_pipeline.py
@@ -1,11 +1,30 @@
-import numpy as np
+import torch
 from qwen_inference import inference
+from transformers import  Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
+import numpy as np
+import argparse
 
-def main():
-    video_path = r"non_anonymized\non_anonymized\video_env_1\ap_0016.mp4"
-    prompt = "Identify the person that is interacting with the object in the video and if the interaction is lifting or dropping, the format of output should be like {\"bbox_2d\": [x1, y1, x2, y2], \"label\": \"Person\"}"
-    output_text = inference(video_path, prompt)
-    print(output_text)
+def main(video_path):
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    prompt = """
+    Identify the object being lifted in this video. Provide in detail the name of the object.
+    """
+
+    model = Qwen2VLForConditionalGeneration.from_pretrained("Qwen/Qwen2-VL-2B-Instruct", torch_dtype="auto", device_map = device)
+
+    processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-2B-Instruct")
+
+    output_text = inference(model, processor, video_path, prompt, device)
+
+    print(f"Extracted item: {output_text}")
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Run video inference")
+
+    parser.add_argument("--video_path", type=str, required=True, help="Path to the input video")
+
+    args = parser.parse_args()
+
+    main(args.video_path)
